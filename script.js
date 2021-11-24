601d14f9648a43880340c83df48ecfbf987c2a34
Matthews Britto <84083804+MatthewsBritto@users.noreply.github.com>
2021-11-24 13:37:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -51,3 +51,79 @@ const addOp = (evento) => {
 
 //identifica qual botao operador foi pressionado...
 sinals.forEach (operador => operador.addEventListener('click',addOp))
+
+const resultClick = () =>{
+    calcular();
+    Op = undefined;
+}
+document.getElementById("result").addEventListener('click', resultClick);
+
+const clean = () => display.textContent = "";
+document.getElementById('clean').addEventListener('click',clean)
+
+const turnOff = () => {
+    clean();
+    novo_Num = true;
+    Op = undefined;
+    n1 = undefined;
+}
+document.getElementById('turnOff').addEventListener('click', turnOff)
+
+
+const backspace = () => display.textContent = display.textContent.slice(0, -1);
+document.getElementById('backspace').addEventListener('click', backspace);
+
+
+const invertSnl = () => {
+        novo_Num = true;
+        atualizarDisplay (display.textContent*-1);
+}
+document.getElementById('invert').addEventListener('click', invertSnl)
+
+
+const existDcm = () => display.textContent.indexOf('.') !== -1;
+const existVlr = () => display.textContent.length > 0 ;
+const insertDcm = () => {
+    if (!existDcm()){
+        if (existVlr()){
+            atualizarDisplay('.');
+        }else{
+            atualizarDisplay('0.');
+        }
+    }
+}
+document.getElementById('decimal').addEventListener('click', insertDcm);
+
+
+//Mapeamento para usar o teclado...
+
+const mapTec = {
+    '0' : 'tc0',
+    '1' : 'tc1',
+    '2' : 'tc2',
+    '3' : 'tc3',
+    '4' : 'tc4',
+    '5' : 'tc5',
+    '6' : 'tc6',
+    '7' : 'tc7',
+    '8' : 'tc8',
+    '9' : 'tc9',
+    '/' : 'op_share',
+    '*' : 'op_mult',
+    '-' : 'op_sub',
+    '+' : 'op_add',
+    '=' : 'result',
+    'Enter' : 'result',
+    'Backspace' : 'backspace',
+    'c' : 'clean',
+    '.' : 'decimal',
+    'Escape' : 'turnOff',
+    'i' : 'invert',
+}
+const mapeandoTec = (evento) => {
+    const tc = evento.key;
+    const tc_mapeado = () => Object.keys(mapTec).indexOf(tc) !== -1;
+    if (tc_mapeado())document.getElementById(mapTec[tc]).click();
+}
+
+document.addEventListener('keydown', mapeandoTec);
